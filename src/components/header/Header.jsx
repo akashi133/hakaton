8a2e0236007a790feecc0f546b5207e078618c0f
Dastan <eles@MacBook-Air-Dastan.local>
2023-02-25 13:25:28 +0600

    headerr

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { logo } from '../../assets/img/logo';
+
+import './header.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header-block">
+      <div className="left">
+        <img src={logo} alt="LOGO" />
+      </div>
+      <div className="center">
+        <ul>
+          <li>ГЛАВНАЯ</li>
+          <li>АУТИЗМ </li>
+          <li>УСЛУГИ</li>
+          <li>example</li>
+        </ul>
+      </div>
+      <div className="right">
+        <button>Авторизоваться</button>
+        <button>Личный кабинет</button>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
